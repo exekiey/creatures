d92a2e35c970bc9f64a7315e4eeabcb20993c46b
exekiey <ezequielcanale@gmail.com>
2024-07-03 17:03:31 +0100

    mira la verdad es que no se

--- creatures/Assets/Prey/FollowCursor.cs
+++ creatures/Assets/Prey/FollowCursor.cs
@@ -7,6 +7,8 @@ public class FollowCursor : MonoBehaviour
 
     Camera main;
 
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -16,6 +18,34 @@ public class FollowCursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 newPosition = main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = newPosition - (Vector2)transform.position;
+
+
+        if (Physics2D.OverlapPoint(newPosition, LayerMask.GetMask("obstacle")))
+        {
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, float.PositiveInfinity, LayerMask.GetMask("obstacle"));
+            Vector2 hitPos = hit.point;
+
+            Vector2 oppositeDirection = direction * -1;
+
+            float aBit = 0.1f;
+
+            Vector2 aBitFurther = oppositeDirection.normalized * aBit;
+
+            transform.position = hitPos + aBitFurther;
+
+        }
+        else
+        {
+            transform.position = newPosition;
+        }
+
+
+
+
+
     }
 }
